031be41340a2f17372fc2cdf63bc7acaf54155d4
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2013-08-06 23:13:16 +0000

    Add FindMediaFileWithKeyword function.
    git-svn-id: file:///Volumes/Buffer%20Disk/r604_python_only_repo/trunk@9 284ab01a-e931-415e-a06d-2cba5beede73

--- automove_to_dir.py
+++ automove_to_dir.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import os,sys,string
+import os,sys,string,re
 
 ARGVS = sys.argv
 ARGC = len(ARGVS)
@@ -10,19 +10,32 @@ OWN = ARGVS[0]
 # sub routine
 ###################
 def GetListOfDirectoryToMove(RootDirectory=""):
-	ListOfDirectoryToMove = ['test1', 'test2', 'test3']
+	ListOfDirectoryToMove = [ \
+			'/root/animal', \
+			'/root/cinema', \
+			'/root/dorama']
 	return ListOfDirectoryToMove
 
+def FindMediaFileWithKeyword(KeyWord=""):
+	ListOfMediaFile = [ \
+			"animalplanet.mp4", \
+			"get_animal_in_fieald.mp4", \
+			"some_animals.mp4"]
+	return ListOfMediaFile
 
 ###################
 # main routine
 ###################
 
 try:
-	print("test")
 	ListOfDir = GetListOfDirectoryToMove(os.path.curdir)
+	
 	for Dir in ListOfDir:
-		print(Dir)
+		ListOfMediaFile = FindMediaFileWithKeyword(Dir)
+		
+		for MediaFile in ListOfMediaFile:
+			print("Keyword is {KeyWord}, Mediafile is {KeyMediaFile}.".format(KeyWord=Dir, KeyMediaFile=MediaFile))
 
 except Exception as err:
 	print("Error type is {ErrType}, {Args}.".format(ErrType=type(err),Args=err.args))
+

300d7cbdc30c2fd6db0afecb1dac2ce953a0e616
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2013-08-10 13:53:02 +0000

    Change the interface of GetListOfDirectoryToMove to get the target directory as dictionary.
    git-svn-id: file:///Volumes/Buffer%20Disk/r604_python_only_repo/trunk@11 284ab01a-e931-415e-a06d-2cba5beede73

--- automove_to_dir.py
+++ automove_to_dir.py
@@ -6,11 +6,15 @@ import os,sys,string,re
 # sub routine
 ###################
 def GetListOfDirectoryToMove(RootDirectory=""):
-	ListOfDirectoryToMove = [ \
-			'/root/animal', \
-			'/root/cinema', \
-			'/root/dorama']
-	return ListOfDirectoryToMove
+	print("Found directory:")
+
+	DictOfDirectoryToMove = {}
+	for FileOrDir in os.listdir(RootDirectory):
+		if os.path.isdir(os.path.join(RootDirectory, FileOrDir)):
+			print(" {Dir}".format(Dir=FileOrDir))
+			DictOfDirectoryToMove[FileOrDir] = os.path.join(RootDirectory, FileOrDir)
+	
+	return DictOfDirectoryToMove
 
 def FindMediaFileWithKeyword(KeyWord=""):
 	ListOfMediaFile = [ \
@@ -27,14 +31,15 @@ try:
 	ARGVS = sys.argv
 	ARGC = len(ARGVS)
 	OWN = ARGVS[0]
+	DIR_MEDIA = ARGVS[1]
+	DIR_TARGET = ARGVS[2]
 	
-	ListOfDir = GetListOfDirectoryToMove(os.path.curdir)
+	DictOfDir = GetListOfDirectoryToMove(DIR_TARGET)
 	
-	for Dir in ListOfDir:
-		ListOfMediaFile = FindMediaFileWithKeyword(Dir)
-		
-		for MediaFile in ListOfMediaFile:
-			print("Keyword is {KeyWord}, Mediafile is {KeyMediaFile}.".format(KeyWord=Dir, KeyMediaFile=MediaFile))
+	for KeyToFind in DictOfDir:
+		print("Key is {Key}, dir is {Dir}.".format(Key=KeyToFind, Dir=DictOfDir[KeyToFind]))
+#		for MediaFile in ListOfMediaFile:
+#			print("Keyword is {KeyWord}, Mediafile is {KeyMediaFile}.".format(KeyWord=Dir, KeyMediaFile=MediaFile))
 
 except Exception as err:
 	print("Error type is {ErrType}, {Args}.".format(ErrType=type(err),Args=err.args))

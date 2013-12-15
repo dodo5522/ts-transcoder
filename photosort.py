f1034377e09b1e578e19ae4d34840f4191d0d21c
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2013-12-15 02:07:44 +0000

    Add empty script for sorting.
    git-svn-id: file:///Volumes/Buffer%20Disk/r604_python_only_repo/trunk@24 284ab01a-e931-415e-a06d-2cba5beede73

---
+++ photosort.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+	module:		photosort
+
+	This script to make directory of date which the photo is taken, and move the photo into the directory.
+
+	argument1:	root directory path of source
+	argument2:	root directory path of destination
+"""
+

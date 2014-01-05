fe26de7c7e18cd976c734e0557010a7a6cac1c5a
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2014-01-05 02:53:40 +0000

    Add PhotoSort class and some methods.
    git-svn-id: file:///Volumes/Buffer%20Disk/r604_python_only_repo/trunk@25 284ab01a-e931-415e-a06d-2cba5beede73

--- photosort.py
+++ photosort.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
 """
 	module:		photosort
@@ -8,4 +8,48 @@
 	argument1:	root directory path of source
 	argument2:	root directory path of destination
 """
+
+import os,glob,sys,string,re
+import pyexiv2 as exiv
+
+class PhotoSort:
+	def __init__(self, aTargetDirectory):
+		print "constructor is called."
+		pathWithFileNamePattern = os.path.join(aTargetDirectory,"*.jpg")
+		self.targetDirectory = aTargetDirectory
+		self.foundFilesPathList = glob.glob(pathWithFileNamePattern)
+		
+	def getPhotoFilesPathList(self):
+		return self.foundFilesPathList
+	
+	def showHelp(self):
+		print "this_script path_to_root_directory"
+	
+	def __del__(self):
+		print "destructor is called."
+
+# main routine for executed as python script
+if __name__ == '__main__':
+	try:
+		if len(sys.argv) != 2:
+			raise ValueError("invalid arguments")
+		elif sys.argv[1] == "error":
+			raise Exception("exception test")
+		else:
+			pathRoot = sys.argv[1]
+		
+		objPhotoSort = PhotoSort(pathRoot)
+		pathsOfPhoto = objPhotoSort.getPhotoFilesPathList()
+		print "Photo files paths are",pathsOfPhoto
+
+	except Exception as err:
+		print "Exception type is ",type(err)
+		print "Exception arg is ",err.args
+		print "Exception is ",err
+	
+	else:
+		print "else"
+	
+	finally:
+		print "finally"
 

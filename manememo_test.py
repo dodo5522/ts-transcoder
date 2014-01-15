60ccc5dcd0a43202368ee432152bb0257c74e62f
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2014-01-15 15:14:16 +0000

    Add manememo_test.py for TDD.
    git-svn-id: file:///Volumes/Buffer%20Disk/r604_python_only_repo/trunk@33 284ab01a-e931-415e-a06d-2cba5beede73

--- manememo_test.py
+++ manememo_test.py
@@ -4,10 +4,35 @@
 import unittest
 from manememo import Manememo
 
+_pathCsvSrcFile = './btmu_torihiki_20140114004100.csv'
+
 class TestManememo(unittest.TestCase):
 	def test_init(self):
-		objManememo = Manememo()
-		self.assertIsInstance(objManememo, Manememo, None)
+		_objManememo = Manememo()
+		self.assertIsNotNone(_objManememo,None)
+		self.assertIsInstance(_objManememo,Manememo,None)
+		pass
+
+	def test_parseCsvFile(self):
+		_objManememo = Manememo()
+		_errorCode = _objManememo.parseCsvFile(_pathCsvSrcFile)
+		self.assertTrue(_errorCode,None)
+		pass
+		
+	def test_getParsedData(self):
+		_objManememo = Manememo()
+		_dataOfBank = []
+		_dataOfOther = []
+		_dataOfStock = []
+		_dataOfCard = []
+		_dataAll = [_dataOfBank,_dataOfOther,_dataOfStock,_dataOfCard]
+		
+		_errorCode = _objManememo.getParsedData(_dataAll)
+		self.assertTrue(_errorCode,None)
+		self.assertGreaterEqual(_dataAll[0],0,None)
+		self.assertGreaterEqual(_dataAll[1],0,None)
+		self.assertGreaterEqual(_dataAll[2],0,None)
+		self.assertGreaterEqual(_dataAll[3],0,None)
 		pass
 
 if __name__ == '__main__':

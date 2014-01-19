26198e16a1746c5ecad79d1f5bcdd7e31de0812f
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2014-01-19 01:19:04 +0000

    To call getParsedData method with new interface.
    git-svn-id: file:///Volumes/Buffer%20Disk/r604_python_only_repo/trunk@37 284ab01a-e931-415e-a06d-2cba5beede73

--- manememo_test.py
+++ manememo_test.py
@@ -8,31 +8,39 @@ _pathCsvSrcFile = './btmu_torihiki_20140114004100.csv'
 
 class TestManememo(unittest.TestCase):
 	def test_init(self):
+		print "test init start."
 		_objManememo = Manememo()
 		self.assertIsNotNone(_objManememo,None)
 		self.assertIsInstance(_objManememo,Manememo,None)
+		print "test init end."
 		pass
 
 	def test_parseCsvFile(self):
+		print "test parseCsvFile start."
 		_objManememo = Manememo()
 		_errorCode = _objManememo.parseCsvFile(_pathCsvSrcFile)
 		self.assertTrue(_errorCode,None)
+		print "test parseCsvFile end."
 		pass
 		
 	def test_getParsedData(self):
+		print "test getParsedData start."
 		_objManememo = Manememo()
-		_dataOfBank = []
-		_dataOfOther = []
-		_dataOfStock = []
-		_dataOfCard = []
-		_dataAll = [_dataOfBank,_dataOfOther,_dataOfStock,_dataOfCard]
-		
-		_errorCode = _objManememo.getParsedData(_dataAll)
+		_errorCode = _objManememo.parseCsvFile(_pathCsvSrcFile)
 		self.assertTrue(_errorCode,None)
-		self.assertGreaterEqual(_dataAll[0],0,None)
-		self.assertGreaterEqual(_dataAll[1],0,None)
-		self.assertGreaterEqual(_dataAll[2],0,None)
-		self.assertGreaterEqual(_dataAll[3],0,None)
+		
+		dataAll = _objManememo.getParsedData()
+		print "all data length : %d" % len(dataAll)
+		self.assertGreater(len(dataAll),0,None)
+		print "bank data length : %d" % len(dataAll[0])
+		self.assertGreater(len(dataAll[0]),0,None)
+		#print "other data length : %d" % len(dataAll[1])
+		#self.assertGreater(len(dataAll[1]),0,None)
+		#print "stock data length : %d" % len(dataAll[2])
+		#self.assertGreater(len(dataAll[2]),0,None)
+		print "credit card length : %d" % len(dataAll[3])
+		self.assertGreater(len(dataAll[3]),0,None)
+		print "test getParsedData end."
 		pass
 
 if __name__ == '__main__':

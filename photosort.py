50e24e2f7ac9331d427068b9d9701f2d36ccadb6
dodo5522 <dodo5522@284ab01a-e931-415e-a06d-2cba5beede73>
2014-04-07 22:27:05 +0000

    phosort.py to sort photo files according to the EXIF tag.
    git-svn-id: file:///Volumes/Buffer%20Disk/r604_python_only_repo/trunk@75 284ab01a-e931-415e-a06d-2cba5beede73

--- photosort.py
+++ photosort.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2.7
 
 """
 	module:		photosort
@@ -10,15 +10,25 @@
 """
 
 import os,glob,sys,string,re
-#import pyexiv2 as exiv
+import shutil
+from exifread.tags import DEFAULT_STOP_TAG, FIELD_TYPES
+from exifread import process_file, __version__
 
 class PhotoSort:
-	_stringPathOfRoot = ''
-	_listExtentions = [] 
-	
 	def __init__(self, stringPathOfRoot, listExtentions):
-		self._stringPathOfRoot = stringPathOfRoot
-		self._listExtentions = listExtentions
+		extentions = []
+		extention_lower = []
+		extention_upper = []
+		
+		for extention in listExtentions:
+			extention_lower.append(extention.lower())
+			extention_upper.append(extention.upper())
+		
+		extentions.extend(extention_lower)
+		extentions.extend(extention_upper)
+		
+		setattr(self, "_listExtentions", extentions)
+		setattr(self, "_stringPathOfRoot", stringPathOfRoot)
 	
 	def getTargetDirectory(self):
 		return self._stringPathOfRoot
@@ -26,23 +36,49 @@ class PhotoSort:
 	def getFileExtentionsList(self):
 		return self._listExtentions
 	
-	def getPhotoFilesPathList(self, isExtentionsLowerOnly):
-		listExtentionsToFind = []
-		listPathOfFiles = []
-		
-		for stringExtention in self._listExtentions:
-			listExtentionsToFind.append(stringExtention.lower())
-			if not isExtentionsLowerOnly:
-				listExtentionsToFind.append(stringExtention.upper())
-		
-		for stringExtention in listExtentionsToFind:
-			stringPathToFind = os.path.join(self._stringPathOfRoot,"*."+stringExtention)
-			listPathOfFiles.append(glob.glob(stringPathToFind))
-		
-		return listPathOfFiles
-	
-	def makeDirAndSortPhotoFiles(self):
-		return True
+	def sortFiles(self):
+		for fileFound in os.listdir(self._stringPathOfRoot):
+			pathSrcImg = os.path.join(self._stringPathOfRoot, fileFound)
+			if not os.path.isfile(pathSrcImg):
+				continue
+			
+			try:
+				objImgFile = None
+				(pathSrcImgWoExt, ext) = os.path.splitext(fileFound)
+				
+				# if error, exception process continue to the next loop.
+				index = self._listExtentions.index(ext[1:])
+				
+				objImgFile = open(pathSrcImg, "rb")
+				exif_data = process_file(objImgFile, stop_tag='EXIF DateTimeOriginal')
+				objImgFile.close()
+				
+				# thumbnail binary data is not used in this script.
+				unusedParam = 'JPEGThumbnail'
+				if unusedParam in exif_data:
+					del exif_data[unusedParam]
+				
+				# EXIF DateTimeOriginal is stored with this format "YYYY:MM:DD HH:MM:SS".
+				dateAndTime = exif_data['EXIF DateTimeOriginal']
+				
+				if hasattr(dateAndTime, 'printable'):
+					# FIXME: want to translate directory name with some optional character.
+					(date, time) = dateAndTime.printable.split(' ')
+					pathDstDir = os.path.join(os.path.dirname(pathSrcImg), date.translate(None, ':'))
+					pathDstImg = os.path.join(pathDstDir, os.path.basename(pathSrcImg))
+					
+					# create directory to move.
+					if not os.path.isdir(pathDstDir):
+						os.mkdir(pathDstDir)
+					shutil.move(pathSrcImg, pathDstImg)
+				
+			except Exception as err:
+				print str(type(err)) + " occurs with message \"" + err.message + "\"."
+				continue
+			
+			finally:
+				if objImgFile is not None:
+					objImgFile.close()
 	
 	def showHelp(self):
 		print "this_script path_to_root_directory"
@@ -53,25 +89,16 @@ class PhotoSort:
 # main routine for executed as python script
 if __name__ == '__main__':
 	try:
-		if len(sys.argv) != 2:
-			raise ValueError("invalid arguments")
-		elif sys.argv[1] == "error":
-			raise Exception("exception test")
-		
-		pathRoot = sys.argv[1]
+		pathRoot = "/Users/takashi/Pictures"
 		extList = ["jpg","png"]
 		
 		objPhotoSort = PhotoSort(pathRoot,extList)
-		print "Photo files paths are",objPhotoSort.getPhotoFilesPathList(False)
+		print objPhotoSort.getFileExtentionsList()
+		
+		objPhotoSort.sortFiles()
+		
 
 	except Exception as err:
 		print "Exception type is ",type(err)
 		print "Exception arg is ",err.args
 		print "Exception is ",err
-	
-	else:
-		print "else"
-	
-	finally:
-		print "finally"
-

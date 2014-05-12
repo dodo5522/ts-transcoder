44f24775003e037cecea4946e2f9089bc06b45d3
Takashi Ando <dodo5522@gmail.com>
2014-05-13 00:31:34 +0900

    Use glob for issue #10.

--- photosort.py
+++ photosort.py
@@ -33,69 +33,70 @@ class PhotoSort:
 		return self._ext_src
 	
 	def sort_files(self):
-		for file_found in os.listdir(self._path_root_src):
-			path_src_img = os.path.join(self._path_root_src, file_found)
-			if not os.path.isfile(path_src_img):
-				continue
-			
-			try:
-				obj_img = None
-				(path_src_img_wo_ext, ext) = os.path.splitext(file_found)
+		for extention in self._ext_src:
+			pattern_search = '*.%s' % extention
+			for path_src_img in glob.glob(os.path.join(self._path_root_src, pattern_search)):
+				if not os.path.isfile(path_src_img):
+					continue
 				
-				#FIXME: issue#8
-				# if the found mediafile is movies, use mediaionfo class method to get encoded date.
-				date = None
-				if ext[1:] in EXT_MOVIES:
-					obj_media = mediainfo.MediaInfo(path_src_img, None)
+				try:
+					obj_img = None
+					(path_src_img_wo_ext, ext) = os.path.splitext(path_src_img)
 					
-					media_data = obj_media.info_video.get_encoded_date()
-					date_and_time = media_data.split()
-					# FIXME: issue#4:  want to translate directory name with some optional character.
-					date = date_and_time[1].replace('-', '')
-				
-				# if the found mediafile is pictures, use exifread to get exif data and taken date.
-				elif ext[1:] in self._ext_src:
-					obj_img = open(path_src_img, "rb")
-					exif_data = process_file(obj_img, stop_tag=TAG_DATE_TIME)
-					obj_img.close()
+					#FIXME: issue#8
+					# if the found mediafile is movies, use mediaionfo class method to get encoded date.
+					date = None
+					if ext[1:] in EXT_MOVIES:
+						obj_media = mediainfo.MediaInfo(path_src_img, None)
+						
+						media_data = obj_media.info_video.get_encoded_date()
+						date_and_time = media_data.split()
+						# FIXME: issue#4:  want to translate directory name with some optional character.
+						date = date_and_time[1].replace('-', '')
 					
-					# thumbnail binary data is not used in this script.
-					tag_unused = 'JPEGThumbnail'
-					if tag_unused in exif_data:
-						del exif_data[tag_unused]
+					# if the found mediafile is pictures, use exifread to get exif data and taken date.
+					elif ext[1:] in self._ext_src:
+						obj_img = open(path_src_img, "rb")
+						exif_data = process_file(obj_img, stop_tag=TAG_DATE_TIME)
+						obj_img.close()
+						
+						# thumbnail binary data is not used in this script.
+						tag_unused = 'JPEGThumbnail'
+						if tag_unused in exif_data:
+							del exif_data[tag_unused]
+						
+						# EXIF DateTimeOriginal is stored with this format "YYYY:MM:DD HH:MM:SS".
+						date_and_time = exif_data[TAG_DATE_TIME]
+						
+						# if date_and_time does not have attribute 'printable', enter to next loop.
+						# FIXME: issue #4: want to translate directory name with some optional character.
+						(date, time) = date_and_time.printable.split(' ')
+						date = date.translate(None, ':')
 					
-					# EXIF DateTimeOriginal is stored with this format "YYYY:MM:DD HH:MM:SS".
-					date_and_time = exif_data[TAG_DATE_TIME]
+					else:
+						msg = 'This extention \'%s\' is not supported.' % ext[1:]
+						raise ValueError(msg)
 					
-					# if date_and_time does not have attribute 'printable', enter to next loop.
-					# FIXME: issue #4: want to translate directory name with some optional character.
-					(date, time) = date_and_time.printable.split(' ')
-					date = date.translate(None, ':')
+					# if destination path is not set, destination is same as source.
+					if self._path_root_dst is not None:
+						path_dst_dir = os.path.join(self._path_root_dst, date)
+					else:
+						path_dst_dir = os.path.join(os.path.dirname(path_src_img), date)
+					path_dst_img = os.path.join(path_dst_dir, os.path.basename(path_src_img))
+					
+					# create directory to move.
+					if not os.path.isdir(path_dst_dir):
+						os.mkdir(path_dst_dir)
+					shutil.move(path_src_img, path_dst_img)
+					
+				except Exception as err:
+					if self._debug == True:
+						print str(type(err)) + " occurs with message \"" + err.message + "\"."
+					continue
 				
-				else:
-					msg = 'This extention \'%s\' is not supported.' % ext[1:]
-					raise ValueError(msg)
-				
-				# if destination path is not set, destination is same as source.
-				if self._path_root_dst is not None:
-					path_dst_dir = os.path.join(self._path_root_dst, date)
-				else:
-					path_dst_dir = os.path.join(os.path.dirname(path_src_img), date)
-				path_dst_img = os.path.join(path_dst_dir, os.path.basename(path_src_img))
-				
-				# create directory to move.
-				if not os.path.isdir(path_dst_dir):
-					os.mkdir(path_dst_dir)
-				shutil.move(path_src_img, path_dst_img)
-				
-			except Exception as err:
-				if self._debug == True:
-					print str(type(err)) + " occurs with message \"" + err.message + "\"."
-				continue
-			
-			finally:
-				if obj_img is not None:
-					obj_img.close()
+				finally:
+					if obj_img is not None:
+						obj_img.close()
 	
 	def __del__(self):
 		print "destructor is called."

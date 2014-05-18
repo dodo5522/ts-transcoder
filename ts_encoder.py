80a9613392de622c39de1d314c9fb52f7c507cbd
Takashi Ando <dodo5522@gmail.com>
2014-05-18 20:43:40 +0900

    Add some basic class to execute each tool for issue #6.

--- ts_encoder.py
+++ ts_encoder.py
@@ -3,5 +3,20 @@
 
 import os,sys,re
 
+class ExecTool(object):
+	pass
+
+class ExecTsSplitter(ExecTool):
+	pass
+
+class ExecCciConv(ExecTool):
+	pass
+
+class ExecMediaCoder(ExecTool):
+	pass
+
+class ExecTrashBox(ExecTool):
+	pass
+
 if __name__ == '__main__':
 	pass

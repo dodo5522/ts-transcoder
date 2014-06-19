06ab9b544b0ab29662c9135d3be5117c4300fba2
Takashi Ando <dodo5522@gmail.com>
2014-06-20 08:03:12 +0900

    Remove unneeded comment on automove.

--- automove_to_dir.py
+++ automove_to_dir.py
@@ -46,11 +46,9 @@ if __name__ == '__main__':
 	try:
 		ARGVS = sys.argv
 		ARGC = len(ARGVS)
-		OWN = ARGVS[0]
 		
 		if ARGC != 3:
 			print("Error! Argument is not enough.")
-			print("{Own} dir_of_src dir_of_dst(includes key directories)".format(Own=OWN))
 			sys.exit()
 		
 		DIR_MEDIA = ARGVS[1]

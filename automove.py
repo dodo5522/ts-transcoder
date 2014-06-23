91c1c926362dcec3dfda797c504c14b18557a1f4
Takashi Ando <dodo5522@gmail.com>
2014-06-24 07:38:42 +0900

    Implement complete constructer of AutoMove class to fix issue #38.

--- automove.py
+++ automove.py
@@ -10,26 +10,57 @@ DESCRIPTION
 	The target directories' name is the keyword to find media files.
 '''
 class AutoMove(object):
-	def get_dict_dirs(self, path_root):
-		dict_dirs = {}
-		for dir_found in os.listdir(path_root):
-			if os.path.isdir(os.path.join(path_root, dir_found)):
-				dict_dirs[dir_found] = os.path.join(path_root, dir_found)
+	def __init__(self, path_src_root, path_dest_root):
+		list_dest_paths = []
+		for dir_found in os.listdir(path_dest_root):
+			if os.path.isdir(os.path.join(path_dest_root, dir_found)):
+				list_dest_paths.append(os.path.join(path_dest_root, dir_found))
 		
-		logging.debug("Found directory:{FOUND_KEYS}".format(FOUND_KEYS=dict_dirs.keys()))
-		return dict_dirs
-	
-	def get_dict_mediafiles(self, path_root, keyword):
+		logging.debug("Found directory:{FOUND_KEYS}".format(FOUND_KEYS=list_dest_paths))
+		setattr(self, "_list_dest_paths", list_dest_paths)
+		
 		dict_mediafiles = {}
-		for file_found in glob.glob(os.path.join(path_root, '*.mp4')):
-			if os.path.isfile(file_found):
-				objre = re.search(keyword, file_found)
-				if objre != None:
-					file_name = os.path.basename(file_found)
-					dict_mediafiles[file_name] = os.path.join(file_found)
+		for path_dest in self._list_dest_paths:
+			list_mediafiles = []
+			keyword = os.path.basename(path_dest)
+			for file_found in glob.glob(os.path.join(path_src_root, '*' + keyword + '*.mp4')):
+				if not os.path.isfile(file_found):
+					continue
+				list_mediafiles.append(file_found)
+			dict_mediafiles[keyword] = list_mediafiles
 		
-		logging.debug("Found media:{FOUND_MEDIA}".format(FOUND_MEDIA=dict_mediafiles.keys()))
-		return dict_mediafiles
+		logging.debug("Found media:{FOUND_MEDIA}".format(FOUND_MEDIA=dict_mediafiles.items()))
+		setattr(self, "_dict_mediafiles", dict_mediafiles)
+	
+	def get_dest_dirs(self):
+		return self._list_dest_paths
+	
+	def get_dict_mediafiles(self):
+		return self._dict_mediafiles
+	
+	def move_mediafiles(self):
+		for path_dest in self._list_dest_paths:
+			keyword = os.path.basename(path_dest)
+			logging.debug("Target dir:{PATH_TARGET}".format(PATH_TARGET=path_dest))
+			
+			for path_media_src in self._dict_mediafiles[keyword]:
+				logging.debug("Found media:{PATH_MEDIA}".format(PATH_MEDIA=path_media_src)) 
+				path_media_dst = os.path.join(path_dest, os.path.basename(path_media_src))
+				
+				if os.path.isfile(path_media_dst):
+					logging.warn("{PATH_MEDIA} already exists so remove it.".format(PATH_MEDIA=path_media_dst))
+					os.remove(path_media_src)
+				else:
+					tmp_path_media = path_media_src
+					tmp_path_target = path_dest
+					if platform.system() == 'Darwin':
+						file_media_unicode = tmp_path_media.decode('utf-8')
+						dir_found_unicode = tmp_path_target.decode('utf-8')
+						tmp_path_media = unicodedata.normalize('NFC', file_media_unicode).encode('utf-8')
+						tmp_path_target = unicodedata.normalize('NFC', dir_found_unicode).encode('utf-8')
+					logging.info("Move {PATH_MEDIA} to {PATH_TARGET}".format(PATH_MEDIA=tmp_path_media, PATH_TARGET=tmp_path_target))
+					
+					shutil.move(path_media_src, path_media_dst)
 
 if __name__ == '__main__':
 	try:
@@ -44,32 +75,10 @@ if __name__ == '__main__':
 		path_root_moving = argvs[2]
 		
 		logging.basicConfig(level=logging.DEBUG)
-		obj = AutoMove()
-		dict_dirs_moving = obj.get_dict_dirs(path_root_moving)
 		
-		for dir_found in dict_dirs_moving:
-			logging.debug("Target dir:{DIR_TARGET}".format(DIR_TARGET=dir_found))
-			dict_mediafiles = obj.get_dict_mediafiles(path_mediafiles_located, dir_found)
-			
-			for file_media in dict_mediafiles:
-				logging.debug("Found media:{FILE_MEDIA}".format(FILE_MEDIA=file_media)) 
-				path_media_src = dict_mediafiles[file_media]
-				path_media_dst = os.path.join(dict_dirs_moving[dir_found],file_media)
-				
-				if os.path.isfile(path_media_dst):
-					logging.warn("{FILE_MEDIA} already exists so remove it.".format(FILE_MEDIA=path_media_dst))
-					os.remove(path_media_src)
-				else:
-					tmp_file_media = file_media
-					tmp_dir_found = dir_found
-					if platform.system() == 'Darwin':
-						file_media_unicode = tmp_file_media.decode('utf-8')
-						dir_found_unicode = tmp_dir_found.decode('utf-8')
-						tmp_file_media = unicodedata.normalize('NFC', file_media_unicode).encode('utf-8')
-						tmp_dir_found = unicodedata.normalize('NFC', dir_found_unicode).encode('utf-8')
-					logging.info("Move {FILE_MEDIA} to {DIR_TARGET}".format(FILE_MEDIA=tmp_file_media, DIR_TARGET=tmp_dir_found))
-					shutil.move(path_media_src, path_media_dst)
-	
+		obj = AutoMove(path_mediafiles_located, path_root_moving)
+		obj.move_mediafiles()
+		
 	except Exception as err:
 		traceback.print_exc()
 

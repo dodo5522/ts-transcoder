108cfc7d60302e933884c9a4f779112315657e4a
Takashi Ando <dodo5522@gmail.com>
2014-07-05 14:25:33 +0900

    Make automove.py to use unicode.

--- automove.py
+++ automove.py
@@ -2,8 +2,8 @@
 
 import os, glob, shutil
 import argparse
-import platform, unicodedata
 import logging, traceback
+from conv_uni import *
 
 '''
 DESCRIPTION
@@ -18,7 +18,7 @@ class AutoMove(object):
             path_found = os.path.join(self._path_dest_root, dir_found)
             if os.path.isdir(path_found):
                 self._dict_paths_dest[dir_found] = path_found
-        logging.debug("Found dirs:{DIRS}".format(DIRS=','.join(self._dict_paths_dest.keys())))
+        logging.debug(u"Found dirs:{DIRS}".format(DIRS=','.join(self._dict_paths_dest.keys())))
 
     def move(self, path_src_file):
         for dir_target in self._dict_paths_dest:
@@ -33,33 +33,28 @@ class AutoSearchMove(AutoMove):
         self._dict_mediafiles = {}
         for keyword in self._dict_paths_dest:
             list_mediafiles = []
-            for file_found in glob.glob(os.path.join(path_src_root, '*' + keyword + '*.mp4')):
+            for file_found in glob.glob(os.path.join(path_src_root, u'*' + keyword + u'*.mp4')):
                 if not os.path.isfile(file_found):
                     continue
                 list_mediafiles.append(file_found)
             self._dict_mediafiles[keyword] = list_mediafiles
-        logging.debug("Found media:{MEDIA}".format(MEDIA=','.join(self._dict_mediafiles.keys())))
+        logging.debug(u"Found media:{MEDIA}".format(MEDIA=','.join(self._dict_mediafiles.keys())))
 
-    def move_mediafiles(self):
+    def move(self):
         for keyword in self._dict_paths_dest:
-            logging.debug("Target dir:{PATH_TARGET}".format(PATH_TARGET=self._dict_paths_dest[keyword]))
+            logging.debug(u"Target dir:{PATH_TARGET}".format(PATH_TARGET=self._dict_paths_dest[keyword]))
 
             for path_media_src in self._dict_mediafiles[keyword]:
-                logging.debug("Found media:{PATH_MEDIA}".format(PATH_MEDIA=path_media_src)) 
+                logging.debug(u"Found media:{PATH_MEDIA}".format(PATH_MEDIA=path_media_src)) 
                 path_media_dst = os.path.join(self._dict_paths_dest[keyword], os.path.basename(path_media_src))
 
                 if os.path.isfile(path_media_dst):
-                    logging.warn("{PATH_MEDIA} already exists so remove it.".format(PATH_MEDIA=path_media_dst))
+                    logging.warn(u"{PATH_MEDIA} already exists so remove it.".format(PATH_MEDIA=path_media_dst))
                     os.remove(path_media_src)
                 else:
                     tmp_path_media = path_media_src
                     tmp_path_target = self._dict_paths_dest[keyword]
-                    if platform.system() == 'Darwin':
-                        file_media_unicode = tmp_path_media.decode('utf-8')
-                        dir_found_unicode = tmp_path_target.decode('utf-8')
-                        tmp_path_media = unicodedata.normalize('NFC', file_media_unicode).encode('utf-8')
-                        tmp_path_target = unicodedata.normalize('NFC', dir_found_unicode).encode('utf-8')
-                    logging.info("Move {PATH_MEDIA} to {PATH_TARGET}".format(PATH_MEDIA=tmp_path_media, PATH_TARGET=tmp_path_target))
+                    logging.info(u"Move {PATH_MEDIA} to {PATH_TARGET}".format(PATH_MEDIA=tmp_path_media, PATH_TARGET=tmp_path_target))
 
                     shutil.move(path_media_src, path_media_dst)
 
@@ -100,12 +95,14 @@ if __name__ == '__main__':
             logging.basicConfig(level=logging.INFO)
 
         if args.path_source_file is not None:
-            obj_automv = AutoMove(args.path_destination_dir)
-            obj_automv.move(args.path_source_file)
+            obj_automv = AutoMove(str_to_uni(args.path_destination_dir))
+            obj_automv.move(str_to_uni(args.path_source_file))
 
         if args.path_source_dir is not None:
-            obj_automv = AutoSearchMove(args.path_destination_dir, args.path_source_dir)
-            obj_automv.move_mediafiles()
+            obj_automv = AutoSearchMove(\
+                    str_to_uni(args.path_destination_dir), \
+                    str_to_uni(args.path_source_dir))
+            obj_automv.move()
 
     except Exception as err:
         traceback.print_exc()
